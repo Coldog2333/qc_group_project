90b6184763401df138620d35e17829d82e9733c4
Coldog2333 <a412133593@gmail.com>
2020-07-08 15:35:58 +0800

    add kaggle submission tool

---
+++ utils.py
@@ -0,0 +1,8 @@
+def record_test_result_for_kaggle(y_pred, submission_file):
+    start = 892
+    end = 1309
+    result = "PassengerId,Survived\n"
+    for i in range(start, end + 1):
+        result += "%d,%d\n" % (i, y_pred[i - 892])
+    with open(submission_file, "w", encoding="utf-8") as writer:
+        writer.write(result)
